2c9d3769c9931bdd18512a70165bb7ec4947e3c9
lidope <mxsxin@yeah.net>
2021-02-07 16:42:28 +0800

    增加字体、更新版本更新提示

--- v2/view/app.js
+++ v2/view/app.js
@@ -114,14 +114,12 @@ App({
         updateManager.onUpdateReady(function () {
             wx.showModal({
                 title: '更新提示',
-                content: '新版本已经准备好，是否重启应用？',
-                success: function (res) {
-                    if (res.confirm) {
-                        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-                        updateManager.applyUpdate()
-                    }
-                }
-            })
+                content: '新版本已经准备好，请重启应用',
+                showCancel: false,
+                success() {
+                    updateManager.applyUpdate()
+                },
+            });
         });
 
         updateManager.onUpdateFailed(function () {
