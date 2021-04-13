8052e4fac94d848cf9c30490e0de81c02cb22b20
lidope <mxsxin@yeah.net>
2021-04-13 16:04:32 +0800

    小程序授权改版

--- v2/view/utils/authorize.js
+++ v2/view/utils/authorize.js
@@ -2,40 +2,62 @@ import { getAuth } from '../api/api';
 export function getAuthorize(e) {
     var { token } = getApp().globalData;
     return new Promise((reslove, reject) => {
-        if (e.type == "getuserinfo") {
-            if (token) {
-                reslove();
-            } else {
-                var info = e.detail;
-                if (!info.iv) {
-                    getApp().error('授权失败');
-                    reject('授权失败');
-                } else {
-                    wx.login({
-                        success(res) {
-                            var code = res['code'];
-                            if (code) {
-                                wx.getUserInfo({
-                                    success: function (userData) {
-                                        var iv = userData['iv'];
-                                        var encryptedData = userData['encryptedData'];
-                                        var params = {
-                                            code: code,
-                                            iv: iv,
-                                            encryptedData: encryptedData
-                                        };
+        if (token && token.indexOf('open_') == -1) {
+            reslove();
+        } else {
+            if (wx.getUserProfile) {
+                console.log('支持wx.getUserProfile')
 
-                                        getAuth(params).then(res => {
-                                            wx.setStorageSync('token', res.d.token);
-                                            getApp().globalData.token = res.d.token;
-                                            reslove(res);
-                                        })
-                                    }
+                wx.getUserProfile({
+                    desc: '用于完善用户资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+                    complete: (userData) => {
+                        console.log(userData)
+
+                        if (userData.errMsg === 'getUserProfile:ok') {
+                            wx.login({
+                                success(loginData) {
+                                    console.log(loginData)
+                                    var params = {
+                                        code: loginData['code'],
+                                        avatar: userData.userInfo.avatarUrl,
+                                        nickname: userData.userInfo.nickName
+                                    };
+
+                                    getAuth(params).then(res => {
+                                        wx.setStorageSync('token', res.d.token);
+                                        getApp().globalData.token = res.d.token;
+                                        reslove(res);
+                                    })
+                                },
+                            })
+                        } else {
+                            getApp().error('授权失败');
+                            reject('授权失败');
+                        }
+                    }
+                })
+            } else {
+                console.log('不支持wx.getUserProfile')
+                wx.login({
+                    success(loginData) {
+                        wx.getUserInfo({
+                            success: function (userData) {
+                                var params = {
+                                    code: loginData['code'],
+                                    avatar: userData.userInfo.avatarUrl,
+                                    nickname: userData.userInfo.nickName
+                                };
+
+                                console.log(userData)
+                                getAuth(params).then(res => {
+                                    wx.setStorageSync('token', res.d.token);
+                                    getApp().globalData.token = res.d.token;
+                                    reslove(res);
                                 })
                             }
-                        }
-                    })
-                }
+                        })
+                    }
+                })
             }
         }
     })
